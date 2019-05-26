0add240a6b499252eb756cd946a6b9f83c6ca5e0
Matheus <matheushr97@gmail.com>
2019-05-26 16:59:58 -0300

    Ajustes finais thumbnail

--- thumbnail-photo/service/thumbnailService.js
+++ thumbnail-photo/service/thumbnailService.js
@@ -3,16 +3,13 @@ const jimp = require('jimp');
 const { BUCKET_THUMBNAIL } = process.env
 
 const thumbnail = async event => {
-    console.log(event)
-    console.log(event.s3)
-    const s3Info = event.s3
+    const s3Info = event.Records[0].s3
     const bucket = s3Info.bucket.name
     const { key } = s3Info.object
 
     const s3Object = await s3Service.getObject(bucket, key)
-    console.log('s3 object', s3Object)
-    const img = await jimp.read(s3Object)
-    const buffer = await img.resize(100, 100).quality(80).getBufferAsync(jimp.MIME_JPEG)
+    const img = await jimp.read(s3Object.Body)
+    const buffer = await img.resize(100, 100).quality(100).getBufferAsync(jimp.MIME_JPEG)
     await s3Service.putObject(BUCKET_THUMBNAIL, key, buffer)
 }
 
