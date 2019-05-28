5553de41bc357436fd9a22d8a0dec15384b08bc6
Matheus.Raymundo <Matheus.Raymundo@senior.com.br>
2019-05-28 13:51:55 -0300

    Impl download da foto

---
+++ download-photo/handler.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const s3Service = require('./service/s3Service')
+const { BUCKET, BUCKET_THUMBNAIL } = process.env
+
+module.exports.download = async (event) => {
+  const { key } = event.pathParameters
+  const { thumbnail } = event.queryStringParameters || {}
+  const bucket = thumbnail && thumbnail === 'true' ? BUCKET_THUMBNAIL : BUCKET
+  const data = await s3Service.getObject(bucket, key)
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'image/jpeg'
+    },
+    body: data.toString('base64'),
+    isBase64Encoded: true
+  };
+
+};
